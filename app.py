98e04d4943c6606116c2defa40fab5c95890b259
codeRaptor89 <marcosecheverria2809@gmail.com>
2025-07-22 14:42:34 -0500

    Ajustes para Render: uso de eventlet

--- app.py
+++ app.py
@@ -332,6 +332,8 @@ def obtener_canal_m3u8():
     return {"url": data["url"]}
 
 if __name__ == "__main__":
+    import eventlet
+    eventlet.monkey_patch() 
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port)
 
