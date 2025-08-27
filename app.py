ec8b6a6b985bee73fa1fdf05c3a2da3a936c572b
codeRaptor89 <marcosecheverria2809@gmail.com>
2025-08-26 20:11:59 -0500

    Actualización del proyecto

--- app.py
+++ app.py
@@ -282,4 +282,4 @@ def registrar_nombre():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
